c1a809419a4cbe9948dedb624383fcb973c08ba8
Yuriy Boychenko <yuboychenko@gmail.com>
2019-04-11 16:13:14 +0300

    Codesniffer fixes

--- src/Event/MultiversionManagerEvent.php
+++ src/Event/MultiversionManagerEvent.php
@@ -6,21 +6,21 @@ use Drupal\multiversion\MultiversionMigrationInterface;
 use Symfony\Component\EventDispatcher\Event;
 
 /**
- *
+ * MultiversionManagerEvent class.
  */
 class MultiversionManagerEvent extends Event {
 
   protected $entityTypes;
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function __construct($entity_types, MultiversionMigrationInterface $migration) {
     $this->entityTypes = $entity_types;
   }
 
   /**
-   *
+   * {@inheritdoc}
    */
   public function getEntityTypes() {
     return $this->entityTypes;
--- src/Event/MultiversionManagerEvents.php
+++ src/Event/MultiversionManagerEvents.php
@@ -3,9 +3,10 @@
 namespace Drupal\multiversion\Event;
 
 /**
- *
+ * MultiversionManagerEvents class.
  */
 class MultiversionManagerEvents {
+
   const PREMIGRATE = 'pre_migrate';
 
   const POSTMIGRATE = 'post_migrate';
